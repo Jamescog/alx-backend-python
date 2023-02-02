95abc081457bf0380cce64fb030f96f936ce4c41
Jamescog <jamescog72@gmail.com>
2023-02-02 19:10:43 +0300

    task 0 completed

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Task 0: Basic annotations -add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ type-annoted function
+       Args:
+        a:float - first number
+        b:float - second number
+       Return:
+        sum of the two numbers
+    """
+
+    return a+b
