3024535e8bf17e8c938efabc9afd63539b376a9f
Jamescog <jamescog72@gmail.com>
2023-02-02 19:25:24 +0300

    task 2 completed

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotations -floor"""
+
+
+def floor(n: float) -> float:
+    """ type annotated function that takes n as argument
+        and returns the floor of the float
+
+        Args:
+            n: float - the float number
+        Returns:
+            floor: float - the floor of the number
+    """
+
+    return float(int(n))
