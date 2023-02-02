ccac07fad948d3b2721ad43218ae0898519f0ab0
Jamescog <jamescog72@gmail.com>
2023-02-02 19:15:58 +0300

    task 1 completed

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotations -concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ type-annotated function that takes a string
+        str1 and str2 and return a concatenated string
+        Args:
+            str1: str - first string
+            str2: str - second string
+        Returns:
+            concatenated string of two
+    """
+
+    return str1 + str2
