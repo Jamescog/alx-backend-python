12be76fb6da7f02b9fe0d572d53e9ac016e3484b
Jamescog <jamescog72@gmail.com>
2023-02-02 19:36:14 +0300

    task 3 completed

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basic annotations -to string"""
+
+
+def to_str(n: float) -> str:
+    """ type-annnoted function that takes a float n as argument
+        and returns the string representation of the float
+
+        Args:
+            n: float - number
+        Returns:
+            string repr of the number
+    """
+    return str(n)
