6a09c8150a86956d56a0caf92119cf0b21944bc1
Jamescog <jamescog72@gmail.com>
2023-02-06 18:48:54 +0300

    task 1 completed

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python4
+"""0. The basics of async"""
+
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """ asynchronous coroutine that takes an integer argument
+        and waits for for a randm delay  between 0 and max-delay
+
+        Args:
+            max_delay -  the time it waits
+        Returns:
+            The the delay time
+    """
+
+    t = random.uniform(0, max_delay)
+    await asyncio.sleep(t)
+    return t
